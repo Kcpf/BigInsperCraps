9e2c227ea979e11585bb8d1412c437626fb9a193
Kcpf <fe22122002@gmail.com>
2020-03-30 11:23:24 -0300

    Recomeço 3

--- craps.py
+++ craps.py
@@ -1,9 +1,15 @@
 import random
 
-def stats(dado1, dado2, soma):
-    print(f"Dado 1: {dado1} e Dado 2: {dado2}")
-    print(f"Soma: {soma}")
-    return None
+"""
+*************************Regras**********************************
+Para escolher um tipo de aposta, utilize as seguintes abreviações:
+Para apostar no Pass Line, digite PLB;
+Para apostar no Field, digite F;
+Para apostar no Any Craps, digite AC;
+Para apostar no Twelve, digite T;
+Obs: É possivel apostar em até 4 tipos de apostas diferentes, dependendo da fase do jogo
+Para fazer mais de uma aposta, utilize o formato TIPO_DE_APOSTA_1,TIPO_DE_APOSTA_2,TIPO_DE_APOSTA_3
+"""
 
 def dados():
     dado1 = random.randint(1, 6)
@@ -13,14 +19,20 @@ def dados():
     while True:
         if input("Aperte F para rodar dado? ") == "f": break
 
+    print(f"Dado 1: {dado1} e Dado 2: {dado2}")
+    print(f"Soma: {soma}")
+
     return (dado1, dado2, dado1+dado2)
 
 def passLineBet(soma):
     aposta = float(input("Quanto deseja apostar no Pass Line? "))
     
-    if soma in [2, 3, 12]:
+    if soma in [7, 11]:
+        print(f"Você ganhou {aposta}!")
+    elif soma in [2, 3, 12]:
         aposta = -aposta
-    elif soma in [4, 5, 6, 8, 9, 10]:
+        print(f"Você perdeu! :(")
+    else:
         print("Você foi para o Point!")
         return (True, soma, aposta)
     
@@ -30,12 +42,16 @@ def field(soma):
     aposta = float(input("Quanto deseja apostar no Field? "))
 
     if soma in [5, 6, 7, 8]:
+        print(f"Você perdeu! :(")
         return -aposta
     elif soma in [3, 4, 9, 10, 11]:
+        print(f"Você ganhou {aposta}!")
         return aposta
     elif soma == 2:
+        print(f"Você ganhou {aposta*2}!")
         return aposta * 2
     elif soma == 12:
+        print(f"Você ganhou {aposta*3}!")
         return aposta * 3       
 
 def anyCraps(soma):
@@ -43,8 +59,10 @@ def anyCraps(soma):
     
     if soma in [2, 3, 12]:
         aposta *= 7
+        print(f"Você ganhou {aposta}!")
     else:
         aposta = -aposta
+        print(f"Você perdeu! :(")
     return aposta
 
 def twelve(soma):
@@ -52,17 +70,18 @@ def twelve(soma):
     
     if soma == 12:
         aposta *= 30
+        print(f"Você ganhou {aposta}!")
     else:
         aposta = -aposta
+        print(f"Você perdeu! :(")
     return aposta
 
 
 def comeOut():
-    nomesDasApostas = ['Pass Line Bet', 'Field', 'Any Craps', 'Twelve']
-    tiposDeAposta = input("Em qual quer apostar (N para não apostar e sair do jogo)? Ex: PLB,F,AC,T   Digite: ").split(",")
+    tiposDeAposta = input("Em qual quer apostar (N para não apostar e sair do jogo)? Ex: PLB,F,AC,T").split(",")
     
     while tiposDeAposta[0] not in ["N", "PLB", "F", "AC", "T"]:
-        tiposDeAposta = input("Em qual quer apostar (N para não apostar e sair do jogo)? Ex: PLB,F,AC,T    Digite: ").split(",")
+        tiposDeAposta = input("Em qual quer apostar (N para não apostar e sair do jogo)? Ex: PLB,F,AC,T").split(",")
     
     if "N" in tiposDeAposta:
         return ('f', [])
@@ -72,7 +91,7 @@ def comeOut():
 
     dic = {"PLB": passLineBet, "F": field, "AC": anyCraps, "T": twelve}
     
-    lucroOuPrejuizo = []
+    lucroOuPrejuizo = 0
     lista = [False, soma, 0]
 
     for aposta in tiposDeAposta:
@@ -81,27 +100,17 @@ def comeOut():
             lista = dic[aposta](soma) # Lista => [bol, soma, aposta]
         
             if lista[0] == False:
-                lucroOuPrejuizo.append(lista[2])
-            else:
-                lucroOuPrejuizo.append(-lista[2])
+                valor += lista[2]
         else:
-            lucroOuPrejuizo.append(dic[aposta](soma))
-    
-    stats(dado1, dado2, soma)
-    
-    for e in range(len(lucroOuPrejuizo)):
-        if lucroOuPrejuizo[e] > 0:
-            print(f"Você ganhou {abs(lucroOuPrejuizo[e])} na aposta {tiposDeAposta[e]}")
-        elif lucroOuPrejuizo[e] == 0:
-            print(f"Você não ganhou nada na aposta {tiposDeAposta[e]}")
-        else:
-            print(f"Você perdeu {abs(lucroOuPrejuizo[e])} na aposta {tiposDeAposta[e]}")
-    
+            valor = dic[aposta](soma)
+
+        lucroOuPrejuizo += valor
 
     
-    return (sum(lucroOuPrejuizo), lista)
+    return (lucroOuPrejuizo, lista)
 
-
+    
+    
 
 
 def point(somaPoint, aposta, din):
@@ -114,34 +123,28 @@ def point(somaPoint, aposta, din):
         
         (dado1, dado2, soma) = dados()
 
-        if tiposDeAposta[0] != "N":
-            dic = {"F": field, "AC": anyCraps, "T": twelve}
-            
-            lucroOuPrejuizo = []
+        dic = {"F": field, "AC": anyCraps, "T": twelve}
+        
+        lucroOuPrejuizo = 0
 
-            for aposta in tiposDeAposta:
-                lucroOuPrejuizo.append(dic[aposta](soma))
-            
-            stats(dado1, dado2, soma)
-            
-            for e in range(len(lucroOuPrejuizo)):
-                if lucroOuPrejuizo[e] > 0:
-                    print(f"Você ganhou {abs(lucroOuPrejuizo[e])} na aposta {tiposDeAposta[e]}")
-                elif lucroOuPrejuizo[e] == 0:
-                    print(f"Você não ganhou nada na aposta {tiposDeAposta[e]}")
-                else:
-                    print(f"Você perdeu {abs(lucroOuPrejuizo[e])} na aposta {tiposDeAposta[e]}")   
-        else:
-            stats(dado1, dado2, soma)      
+        for aposta in tiposDeAposta:
+            valor = dic[aposta](soma)
+
+            lucroOuPrejuizo += valor
+        
+        din += lucroOuPrejuizo
+        
             
     if soma == somaPoint:
-        print(f"Você ganhou o Point, recuperou seus {aposta} e voltou para o Come Out!")
-        return din + float(aposta)
+        print("Você ganhou o Point e voltou para o Come Out!")
+        return din + aposta
     else:
         print("Você perdeu o Point e voltou para o Come Out!")
-        return din - float(aposta)
+        return din - aposta
 
 
+
+    
 
 
 din = 100
@@ -153,7 +156,6 @@ while True:
         break
 
     din += lucroOuPrejuizo
-    print(f'Saldo atual:{din}')
 
     if lista[0]: 
         passou = True
