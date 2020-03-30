5cc580a0b1f478536572bf5a13b3b7ad3ab444aa
Kcpf <fe22122002@gmail.com>
2020-03-30 11:42:48 -0300

    Reduzindo o Código

--- craps.py
+++ craps.py
@@ -1,31 +1,37 @@
 import random
 
-dados_desenho = {1:""" -------
+dados_desenho = {1:"""
+ -------
 |       |
 |   *   |
 |       |
  ------- 
-""", 2:""" -------
+""", 2:"""
+ -------
 | *     |
 |       |
 |     * |
  ------- 
-""", 3:""" -------
+""", 3:"""
+ -------
 | *     |
 |   *   |
 |     * |
  -------
- """, 4:""" -------
+ """, 4:"""
+ -------
 | *   * |
 |       |
 | *   * |
  -------
-  """, 5:""" -------
+  """, 5:"""
+ -------
 | *   * |
 |   *   |
 | *   * |
  -------
-  """, 6:""" -------
+  """, 6:"""
+ -------
 | *   * |
 | *   * |
 | *   * |
@@ -43,7 +49,7 @@ def stats(dado1, dado2, soma):
     while True:
         if input("Aperte F para rodar dado? ") == "f": break
 
-    return(f"""{dados_desenho[dado1]}\n{dados_desenho[dado2]}""")
+    return(f"Dado 1: {dados_desenho[dado1]}\n Dado 2: {dados_desenho[dado2]}")
 
 def field(vai_apostar, valor_da_aposta, soma):
 
@@ -56,8 +62,8 @@ def field(vai_apostar, valor_da_aposta, soma):
             return valor_da_aposta * 2
         elif soma == 12:
             return valor_da_aposta * 3
-    else:
-        return 0
+            
+    return 0
 
 def any_craps(vai_apostar, valor_da_aposta, soma):
 
@@ -66,8 +72,8 @@ def any_craps(vai_apostar, valor_da_aposta, soma):
             return valor_da_aposta * 7
         else:
             return -valor_da_aposta
-    else:
-        return valor_da_aposta
+            
+    return valor_da_aposta
 
 def twelve(vai_apostar, valor_da_aposta, soma):
 
@@ -76,6 +82,7 @@ def twelve(vai_apostar, valor_da_aposta, soma):
             return valor_da_aposta * 30
         else:
             return -valor_da_aposta
+        
     return valor_da_aposta
 
 def pass_line_bet(vai_apostar, valor_da_aposta, soma):
@@ -90,39 +97,37 @@ def pass_line_bet(vai_apostar, valor_da_aposta, soma):
 def come_out(dinheiro):
 
     print('Voce esta na fase de Come Out')
-
-    dinheiro_apostado = 0
-    dinheiro_local = dinheiro
-    teste_aposta = 0
-    aposta_total = 0
-    valida_aposta = False
-    lucro = 0
     vai_para_o_point = False
-    todas_apostas = {"F": field, "AC": any_craps, "T": twelve}
+    aposta_total = 0
+    todas_apostas = {"F": field, "AC": any_craps, "T": twelve, "PLB": pass_line_bet}
     lucro_total = 0
     dic = {}
     quantia_apostada_em_cada_aposta = []
     nomes_das_apostas = {"PLB":"Pass Line Bet", "F":"Field", "AC":"Any Craps", "T":"Twelve"}
+
     tipos_de_aposta = input("Em qual quer apostar (N para não apostar e sair do jogo)? Ex: PLB,F,AC,T   Digite: ").split(",")
     
     while tipos_de_aposta[0] not in ["N", "PLB", "F", "AC", "T"]:
         tipos_de_aposta = input("Em qual quer apostar (N para não apostar e sair do jogo)? Ex: PLB,F,AC,T    Digite: ").split(",")
     
-    tipos_de_aposta.sort()
+
+    if "PLB" in tipos_de_aposta:
+        tipos_de_aposta.remove("PLB")
+        tipos_de_aposta.append("PLB")
 
     (dado1, dado2, soma) = dados()
 
 
     for tipos in tipos_de_aposta:
-        valida_aposta = False
-        while valida_aposta == False:
+        while True:
             teste_aposta = int(input(f'Quanto quer apostar no {nomes_das_apostas[tipos]}?    Digite: '))
             aposta_total+=teste_aposta
-            if aposta_total <= dinheiro_local and aposta_total >= 0:
+
+            if aposta_total <= dinheiro and aposta_total >= 0:
                 quantia_apostada_em_cada_aposta.append(teste_aposta)
-                valida_aposta = True 
+                break
             else:
-                print('aposta invalida')
+                print('Aposta Invalida')
                 aposta_total-=teste_aposta
 
     print(stats(dado1, dado2, soma))
@@ -130,143 +135,102 @@ def come_out(dinheiro):
     for num in range(len(tipos_de_aposta)):
         dic[tipos_de_aposta[num]] = quantia_apostada_em_cada_aposta[num]
 
-    print(quantia_apostada_em_cada_aposta,tipos_de_aposta,dic)
-
-    for tipos in todas_apostas:
-
-        if tipos in tipos_de_aposta:
+    for tipos in tipos_de_aposta:
+        if tipos == "PLB":
+            lucro, vai_para_o_point, soma = pass_line_bet(True, dic[tipos], soma)
+        else:
             lucro = todas_apostas[tipos](True, dic[tipos], soma)
 
-        else:
-            lucro = todas_apostas[tipos](False, 0, soma)
+        lucro_total += lucro
         
         if lucro > 0:
-            lucro_total+=lucro
             print(f'Voce ganhou {lucro} no {nomes_das_apostas[tipos]}')
-
-        elif lucro < 0:
-            lucro_total+=lucro
-            print(f'Voce perdeu {-lucro} no {nomes_das_apostas[tipos]}')
-
-    if "PLB" in tipos_de_aposta:
-        lucro, vai_para_o_point, soma = pass_line_bet(True, dic["PLB"], soma)
-        if lucro > 0:
-            print(f'Voce ganhou {lucro} no {nomes_das_apostas["PLB"]}')
-            lucro_total+=lucro
-        elif lucro < 0 and vai_para_o_point == False:
-            print(f'Voce perdeu {-lucro} no {nomes_das_apostas["PLB"]}')
-            lucro_total+=lucro
+        
         elif lucro < 0 and vai_para_o_point == True:
             print("Você foi para o Point!")
-            dinheiro_apostado = dic["PLB"]
-            lucro_total+=lucro
 
-
-    dinheiro_local+=lucro_total
-
-    return dinheiro_local, vai_para_o_point, soma, dinheiro_apostado
+        elif lucro < 0:
+            print(f'Voce perdeu {-lucro} no {nomes_das_apostas[tipos]}')
+        
     
-    
+    dinheiro+=lucro_total
 
+    return dinheiro, vai_para_o_point, soma, dic["PLB"] if vai_para_o_point else 0
 
-def point(dinheiro, soma_dados_point, dinheiro_apostado):
-    print('Voce esta na fase de Point')
+def point(dinheiro, soma_dados_point, dinheiro_apostado):  
     soma = 0
-    dinheiro_local = dinheiro
-    teste_aposta = 0
     aposta_total = 0
-    valida_aposta = False
-    valida_tipo_aposta = False
-    lucro = 0
     todas_apostas = {"F": field, "AC": any_craps, "T": twelve}
     lucro_total = 0
     dic = {}
     quantia_apostada_em_cada_aposta = []
     nomes_das_apostas = {"F":"Field", "AC":"Any Craps", "T":"Twelve"}
-    siglas_apostas = ["N", "F", "AC", "T"]
     
     while soma not in [7, soma_dados_point]:
+        print('Voce esta na fase de Point')
         
-        teste_aposta = 0
-        aposta_total = 0
-        valida_aposta = False
-        valida_tipo_aposta = False
-        lucro = 0
-        lucro_total = 0
-        dic = {}
-        quantia_apostada_em_cada_aposta = []
-            
+        tipos_de_aposta = input("Em qual quer apostar (N para não apostar)? Ex: F,AC,T   Digite: ").split(",")
     
-        tipos_de_aposta = input("Em qual quer apostar (N para não apostar e sair do jogo)? Ex: F,AC,T   Digite: ").split(",")
-
-        while valida_tipo_aposta == False:
-            for nums in range(len(tipos_de_aposta)):
-                if tipos_de_aposta[nums] not in ["N", "F", "AC", "T"]:
-                    tipos_de_aposta = input("Em qual quer apostar (N para não apostar e sair do jogo)? Ex: F,AC,T    Digite: ").split(",")
-            else:
-                valida_tipo_aposta = True
+        while tipos_de_aposta[0] not in ["N", "PLB", "F", "AC", "T"]:
+            tipos_de_aposta = input("Em qual quer apostar (N para não apostar)? Ex: F,AC,T    Digite: ").split(",")
         
-        tipos_de_aposta.sort()
 
         (dado1, dado2, soma) = dados()
 
+        if tipos_de_aposta[0] != "N":
+            for tipos in tipos_de_aposta:
+                while True:
+                    teste_aposta = int(input(f'Quanto quer apostar no {nomes_das_apostas[tipos]}?    Digite: '))
+                    aposta_total+=teste_aposta
 
-        for tipos in tipos_de_aposta:
-            valida_aposta = False
-            while valida_aposta == False:
-                teste_aposta = int(input(f'Quanto quer apostar no {nomes_das_apostas[tipos]}?    Digite: '))
-                aposta_total+=teste_aposta
-                if aposta_total <= dinheiro_local and aposta_total >= 0:
-                    quantia_apostada_em_cada_aposta.append(teste_aposta)
-                    valida_aposta = True 
-                else:
-                    print('aposta invalida')
-                    aposta_total-=teste_aposta
+                    if aposta_total <= dinheiro and aposta_total >= 0:
+                        quantia_apostada_em_cada_aposta.append(teste_aposta)
+                        break
+                    else:
+                        print('Aposta Invalida')
+                        aposta_total-=teste_aposta
 
-        print(stats(dado1, dado2, soma))
+            print(stats(dado1, dado2, soma))
 
-        for num in range(len(tipos_de_aposta)):
-            dic[tipos_de_aposta[num]] = quantia_apostada_em_cada_aposta[num]
+            for num in range(len(tipos_de_aposta)):
+                dic[tipos_de_aposta[num]] = quantia_apostada_em_cada_aposta[num]
 
-        for tipos in todas_apostas:
+            for tipos in tipos_de_aposta:
 
-            if tipos in tipos_de_aposta:
                 lucro = todas_apostas[tipos](True, dic[tipos], soma)
 
-            else:
-                lucro = todas_apostas[tipos](False, 0, soma)
+                lucro_total += lucro
+                
+                if lucro > 0:
+                    print(f'Voce ganhou {lucro} no {nomes_das_apostas[tipos]}')
+
+                elif lucro < 0:
+                    print(f'Voce perdeu {-lucro} no {nomes_das_apostas[tipos]}')
+                
             
-            if lucro > 0:
-                lucro_total+=lucro
-                print(f'Voce ganhou {lucro} no {nomes_das_apostas[tipos]}')
+            dinheiro+=lucro_total
 
-            elif lucro < 0:
-                lucro_total+=lucro
-                print(f'Voce perdeu {-lucro} no {nomes_das_apostas[tipos]}')
-
-            dinheiro_local+=lucro_total
-
+        else:
+            print(stats(dado1, dado2, soma))
+            print("Voce nao apostou e so rolou os dados")
+        
+        print(f'Seu saldo atual é de {dinheiro} fichas')
 
     if soma == soma_dados_point:
         print(f"Você ganhou o Point, recuperou suas {dinheiro_apostado} fichas apostadas e voltou para o Come Out!")
-        return dinheiro_local + dinheiro_apostado, False
+        return dinheiro + dinheiro_apostado, False
     else:
         print("Você perdeu o Point e voltou para o Come Out!")
-        return dinheiro_local, False
-
-
-
+        return dinheiro, False
 
 def main():
+
     quer_jogar = True
     esta_no_point = False
     dinheiro = 1000
     dinheiro_apostado = 0
-    soma_dados_point = 0
 
-    vamos_jogar = input("""Bem vindo ao Big Insper Craps!
-Vamos jogar?
-digite 'S' para jogar e 'N' para sair    Digite: """)
+    vamos_jogar = input("""Bem vindo ao Big Insper Craps!\nVamos jogar?\nDigite 'S' para jogar e 'N' para sair    Digite: """)
 
     if vamos_jogar == 'N':
         quer_jogar = False
@@ -279,29 +243,27 @@ digite 'S' para jogar e 'N' para sair    Digite: """)
         if dinheiro <= 0 and dinheiro_apostado <= 0:
             print('Que pena! Suas fichas acabaram')
             break
+
         elif not esta_no_point:
-            dinheiro_local, vai_para_o_point, soma, dinheiro_apostado = come_out(dinheiro)
-            dinheiro = dinheiro_local
-            esta_no_point = vai_para_o_point
-            soma_dados_point = soma
+            dinheiro, esta_no_point, soma_dados_point, dinheiro_apostado = come_out(dinheiro)
+
             if dinheiro_apostado == 0:
                 print(f'Seu saldo atual é de {dinheiro} fichas')
             else: 
                 print(f'Seu saldo atual é de {dinheiro} fichas e voce possui {dinheiro_apostado} fichas apostadas')
 
         elif esta_no_point:
-            dinheiro_local, esta_no_point = point(dinheiro, soma_dados_point, dinheiro_apostado)
-            dinheiro = dinheiro_local
-            print(f'Seu saldo atual é de {dinheiro} fichas')
+            dinheiro, esta_no_point = point(dinheiro, soma_dados_point, dinheiro_apostado)
 
 
-        vamos_jogar = input("""Quer continuar jogando?
-digite 'S' para continuar e 'N' para sair    Digite: """)
+        vamos_jogar = input("""Quer continuar jogando?\nDigite 'S' para continuar e 'N' para sair    Digite: """)
 
         if vamos_jogar == 'N':
             quer_jogar = False
             
+    print('Obrigado por jogar! Até a próxima')
+    
+    return None
 
-    
-    
-    return('Obrigado por jogar! Até a próxima')
+
+main()
